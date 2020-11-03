23a5418c8acfe6b038c64ffeb51ff6aa1a1382c9
rlt202 <danik.nikulin@gmail.com>
2020-11-03 16:29:13 +0300

    Initial Commit

---
+++ Table View app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View app
+//
+//  Created by Даниил Никулин on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
